74c1c9d69f5071e1d17099002835915900c8838d
Sugam Karki <sugamkarki7058@gmail.com>
2024-04-29 14:37:28 -0400

    stuffs

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,4 +1,5 @@
 import Navbar from "@/components/Navbar";
+import { Toaster } from "@/components/ui/toaster";
 import { cn } from "@/lib/utils";
 import "@/styles/globals.css";
 import { Inter } from "next/font/google";
@@ -21,10 +22,11 @@ export default function RootLayout({
       )}
     >
       <body className="min-h-screen pt-12 bg-slate-50 antialiased">
-        <Navbar/>
+        <Navbar />
         <div className="container max-w-7xl mx-auto h-full pt-12">
           {children}
         </div>
+        <Toaster />
       </body>
     </html>
   );
--- src/components/UserAuthForm.tsx
+++ src/components/UserAuthForm.tsx
@@ -4,16 +4,23 @@ import { Button } from "./ui/Button";
 import { cn } from "@/lib/utils";
 import { signIn } from "next-auth/react";
 import { Icons } from "./Icons";
+import { useToast } from "@/hooks/use-toast";
 interface UserAuthFormProps extends React.HTMLAttributes<HTMLDivElement> {}
 
 const UserAuthForm: FC<UserAuthFormProps> = ({ className, ...props }) => {
   const [isLoading, setIsLoading] = useState<boolean>(false);
+  const { toast } = useToast();
   const loginWithGoogle = async () => {
     setIsLoading(true);
     try {
+      throw new Error();
       await signIn("google");
     } catch (error) {
-        
+      toast({
+        title: "There was a problem",
+        description: "There was a problem logging on with google",
+        variant: "destructive",
+      });
     } finally {
       setIsLoading(false);
     }
