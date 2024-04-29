67150b21d85494e643e0fada666242b454d416a6
Sugam Karki <sugamkarki7058@gmail.com>
2024-04-29 13:57:12 -0400

    base signin

---
+++ src/components/UserAuthForm.tsx
@@ -0,0 +1,17 @@
+import { FC } from "react";
+import { Button } from "./ui/Button";
+import { cn } from "@/lib/utils";
+
+interface UserAuthFormProps extends React.HTMLAttributes<HTMLDivElement> {}
+
+const UserAuthForm: FC<UserAuthFormProps> = ({ className, ...props }) => {
+  return (
+    <div className={cn("flex justify-center", className)} {...props}>
+      <Button size="sm" className="w-full">
+        Google
+      </Button>
+    </div>
+  );
+};
+
+export default UserAuthForm;
